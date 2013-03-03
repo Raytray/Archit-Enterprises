cfd9c8e884c228b2ce6a995f22abc86d13d4eab1
Andrew McMillion <andrewmcmillion90@gmail.com>
2013-03-03 17:52:53 -0500

    prototype AGAIN

--- Archit_Code/ControlStationTest/src/Driver.java
+++ Archit_Code/ControlStationTest/src/Driver.java
@@ -1,18 +1,34 @@
+import java.util.Random;
 
 public class Driver
 {
-	private static String randomGenerator()
+	static Random generator = new Random();
+	private static String randomString()
 	{
-		
-		return "0";
+
+		char word[] = new char[10];
+		for(int i = 0; i < 10; i++)
+		{
+			word[i] = (char) ((generator.nextInt(26)) + 65);
+		}
+		return String.valueOf(word);
 	}
-	
+
 	public static void main(String[] args)
 	{
+		String temp = "";
 		Robot robot = new Robot();
 		System.out.println("robot.isValidMessage(\"STEELE\")\n" + robot.isValidMessage("STEELE"));
 		System.out.println("robot.isValidMessage(\"MAFR000000\")\n" + robot.isValidMessage("MAFR000000"));
 		System.out.println("robot.isValidMessage(\"MAFR00000\")\n" + robot.isValidMessage("MAFR00000"));
+		for(int i = 0; i < 10; i++)
+		{
+			if(generator.nextInt()%5 == 0)
+			{
+				temp = randomString();
+			}
+			System.out.println("robot.isValidMessage(\"" + temp + "\")\n" + robot.isValidMessage(temp));
+		}
 
 	}
 
