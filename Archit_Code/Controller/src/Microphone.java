b43e5e7171e8841a84e59d998a2fff7519f1cc9d
Raymond Tang <raymond@tang.me>
2013-04-11 18:27:45 -0400

    Removed unnecessary sensor classes.

--- Archit_Code/Controller/src/Microphone.java
+++
@@ -1,7 +0,0 @@
-public class Microphone implements SensorInterface {
-    private int theValue;
-
-    public int getValue() {
-	return theValue;
-    }
-}
--- Archit_Code/Controller/src/TouchSensor.java
+++
@@ -1,7 +0,0 @@
-public class TouchSensor implements SensorInterface {
-    private int theValue;
-
-    public int getValue() {
-	return theValue;
-    }
-}
